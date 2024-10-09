e38fc85d048d1f5db053c867feb399c47499e811
tanphn@decentralab.asia <tanphn@decentralab.asia>
2024-10-09 11:19:03 +0700

    add-deploy-script

--- contracts/scripts/deploy/1_Deploy_CERToken.ts
+++ contracts/scripts/deploy/1_Deploy_CERToken.ts
@@ -2,18 +2,16 @@ import fs from "fs";
 import { ethers } from "hardhat";
 
 async function deployContract() {
-  let cerTokenContract: any  
-  const usdc = ''
+  let cerTokenContract: any;
+  const usdc = "0x036CbD53842c5426634e7929541eC2318f3dCF7e";
 
   try {
-    cerTokenContract = await ethers.deployContract("CERToken", [
-      usdc
-    ]);
+    cerTokenContract = await ethers.deployContract("CERToken", [usdc]);
     await cerTokenContract.waitForDeployment();
 
     console.log("Contracts deployed successfully.");
 
-    return  cerTokenContract ;
+    return cerTokenContract;
   } catch (error) {
     console.error("Error deploying contracts:", error);
 
@@ -31,13 +29,18 @@ async function saveContractAddress(cerTokenContract: any) {
       4
     );
 
-    fs.writeFile("./scripts/deploy/Address_AutoWallet.json", address, "utf8", (error) => {
-      if (error) {
-        console.error("Error saving contract address:", error);
-      } else {
-        console.log("Deployed contract address:", address);
+    fs.writeFile(
+      "./scripts/deploy/Address_CERToken.json",
+      address,
+      "utf8",
+      (error) => {
+        if (error) {
+          console.error("Error saving contract address:", error);
+        } else {
+          console.log("Deployed contract address:", address);
+        }
       }
-    });
+    );
   } catch (error) {
     console.error("Error saving contract address:", error);
 
@@ -51,9 +54,7 @@ async function main() {
   try {
     contract = await deployContract();
 
-    await saveContractAddress(
-      contract
-    );
+    await saveContractAddress(contract);
 
     console.log("Contract deployment completed successfully.");
   } catch (error) {
